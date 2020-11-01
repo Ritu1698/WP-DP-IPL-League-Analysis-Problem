bc4f6b31cf5ca04cdc6cc8bafbcb3d26c7ab832c
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-01 16:47:43 +0530

    [Rituparna]Added: Unit Functions To Find Top Batting Average Cricketer In Sorted Order

--- src/main/java/com/bridgelabz/iplanalyzer/IPLAnalyser.java
+++ src/main/java/com/bridgelabz/iplanalyzer/IPLAnalyser.java
@@ -1,9 +1,15 @@
 package com.bridgelabz.iplanalyzer;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 
 public class IPLAnalyser {
@@ -22,6 +28,39 @@ public class IPLAnalyser {
                     IPLException.ExceptionType.FILE_OR_HEADER_PROBLEM);
         }
 
+    }
+
+    public String getPlayersWithTopAverages() throws IPLException {
+        try (Writer writer = new FileWriter("./src/test/resources/IPLBattingAvg.json")) {
+            if (IPLCSVList == null || IPLCSVList.size() == 0) {
+                throw new IPLException("Empty!!!", IPLException.ExceptionType.NO_DATA);
+            }
+            Comparator<IPLRuns> censusComparator = Comparator.comparing(census -> census.avg);
+            this.descendingSort(censusComparator);
+            String json = new Gson().toJson(IPLCSVList);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(IPLCSVList, writer);
+            return json;
+
+        } catch (RuntimeException | IOException e) {
+            throw new IPLException(e.getMessage(),
+                    IPLException.ExceptionType.FILE_OR_HEADER_PROBLEM);
+        }
+
+
+    }
+
+    private void descendingSort(Comparator<IPLRuns> iplComparator) {
+        for (int i = 0; i < IPLCSVList.size() - 1; i++) {
+            for (int j = 0; j < IPLCSVList.size() - i - 1; j++) {
+                IPLRuns ipl1 = IPLCSVList.get(j);
+                IPLRuns ipl2 = IPLCSVList.get(j + 1);
+                if (iplComparator.compare(ipl1, ipl2) < 0) {
+                    IPLCSVList.set(j, ipl2);
+                    IPLCSVList.set(j + 1, ipl1);
+                }
+            }
+        }
     }
 
 
