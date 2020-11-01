82ebbfb3277f355d3a788a61d7ca1d769554b0bd
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-01 19:13:45 +0530

    [Rituparna]Added: Function to Check Highest Striking Rate And 4w 5w Bowler

--- src/main/java/com/bridgelabz/iplanalyzer/IPLAnalyser.java
+++ src/main/java/com/bridgelabz/iplanalyzer/IPLAnalyser.java
@@ -258,4 +258,23 @@ public class IPLAnalyser {
     }
 
 }
+
+    public String getBowlersWithTopStrikingRateAnd4w5w() throws IPLException{
+        try (Writer writer = new FileWriter("./src/test/resources/IPLBowlingBestSRWithWickets.json")) {
+            if (IPLCSVWickets == null || IPLCSVWickets.size() == 0) {
+                throw new IPLException("Empty!!!", IPLException.ExceptionType.NO_DATA);
+            }
+            Comparator<IPLWickets> iplComparator = Comparator.comparing(IPLWickets::getFourWicket).thenComparing(IPLWickets::getFiveWicket).thenComparing(iplWickets -> iplWickets.strikeRate);
+            this.descendingSortWickets(iplComparator);
+            String json = new Gson().toJson(IPLCSVWickets);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(IPLCSVWickets, writer);
+            return json;
+
+        } catch (RuntimeException | IOException e) {
+            throw new IPLException(e.getMessage(),
+                    IPLException.ExceptionType.FILE_OR_HEADER_PROBLEM);
+        }
+
+    }
 }
